1acc2c3724618dd1b27b3196c511cdd6c0fdd165
beny <jlbeny100@gmail.com>
2017-12-26 19:21:06 +0100

    feat : add unmarshalling

--- src/main/java/com/valoyes/trainings/jaxb/JAXBDemo.java
+++ src/main/java/com/valoyes/trainings/jaxb/JAXBDemo.java
@@ -1,10 +1,12 @@
 package com.valoyes.trainings.jaxb;
 
+import java.io.StringReader;
 import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import com.bharatthippireddy.patient.Patient;
 
@@ -28,6 +30,11 @@ public class JAXBDemo {
 			marshaller.marshal(patient, writer);
 			// mostramos el contenido del writer
 			System.out.println(writer);
+			
+			// ahora queremos unmarshall
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			Patient unmarshalledPatient = (Patient) unmarshaller.unmarshal(new StringReader(writer.toString()));
+			System.out.println("unmarshalledPatient.getName() = " + unmarshalledPatient.getName());
 			
 		} catch (JAXBException e) {
 			e.printStackTrace();
